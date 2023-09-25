07f179941d870061fc397c2d3f4dc27eeb703793
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-09-25 21:33:55 +0200

    ciao

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ const fetchBooks = () => {
 
                        <div class="card-body">
                           <p class="font-weight-bold text-truncate book-title"> ${book.title} </p>
-                          <div class="d-flex justify-content-between align-items-center">                    
-                            <button class="btn btn-danger" onclick="addToCart('${book.title}', '${book.price}', '${book.asin}')"><i class="bi bi-cart"></i> ${book.price} €  </button>
+                          <p class="font-weight-bold book-price"> ${book.price} €</p>
+                          <div class="d-flex justify-content-evenly align-items-center">                    
+                            <button class="btn btn-danger" onclick="addToCart('${book.title}', '${book.price}', '${book.asin}')"><i class="bi bi-cart"></i></button>
                             <button class="btn btn-secondary" onclick="hideCard(event)"><i class="bi bi-x-circle-fill"></i></button>
                            </div>
                         </div>
@@ -50,28 +51,69 @@ const searchBook = (ev) => {
 
   const addToCart = (title, price, asin) => {
     const book = document.querySelector("#book_" + asin)
-    book.style.border = "2px red solid"
+    book.style.opacity = "0.5"
     const cart = document.querySelector(".list-group")
     cart.innerHTML += `
     <li class="list-group-item">${title}, € ${price} 
-    <button class='btn btn-secondary' onclick='removeFromCart(event, "${asin}", "${price}")'> X </button>
+    <button class='btn btn-outline-secondary' onclick='removeFromCart(event, "${asin}", "${price}")'> X </button>
     </li> `
 
     /*const totale = document.querySelector("h1 span")
     totale.innerText = (Number(totale.innerText) + Number(price)).toFixed(2)*/
+  }
 
-    let array = [document.querySelectorAll(".book-price")]
-    let somma = 0
-    array.forEach (Number=> {
-      somma = somma + Number
-  })
-  const totale = document.querySelector("h1 span")
-  totale.innerText = somma
+ /* let cart = [];
+  cart.push()
+  const cartTotal = document.querySelector("h1 span")
+    
+  const total = cart.reduce((total, amount) => total + amount);
+  cartTotal.innerText = total.toFixed(2);*/
+
+  /*function sumPrices(products) {
+    const totalPrice = products.reduce((accumulator, currentProduct) => {
+      return accumulator + currentProduct.price;
+    }, 0);
+    const formattedTotalPrice = totalPrice.toFixed(2);
+    const totalPriceWithEuroSymbol = `€ ${formattedTotalPrice}`;
+  
+    return totalPriceWithEuroSymbol; 
+  }
+
+
+  const updateCartTotal = () => {
+    const cartTotal = document.querySelector("h1 span")
+    const cart = document.querySelector(".list-group ${price}")
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+    total += cart[i].price;
+  }
+  
+  cartTotal.innerText = total.toFixed(2);
   }
    
- 
+  let cart = {};
+  function addToCart2(productName) {
+    if (cart[productName]) {
+      cart[productName]++;
+    } else {
+      cart[productName] = 1;
+    }
+  }
     
+  function calculateTotalPrice() {
+    let totalPrice = 0;
+  
+    for (let productName in cart) {
+      let productQuantity = cart[productName];
+      let productPrice = function getProductPrice(productName) {
+      totalPrice += productQuantity * productPrice;
+    }}
+  
+    return totalPrice;
+  }
 
+  let totalCartElement = document.getElementById("h1 span");
+totalCartElement.textContent = calculateTotalPrice();*/
 
  
 
@@ -80,14 +122,14 @@ const searchBook = (ev) => {
     const totale = document.querySelector("h1 span")
     totale.innerText = (Number(totale.innerText) - Number(price)).toFixed(2)
     const book = document.querySelector("#book_" + asin)
-    book.style.border = "none"
+    book.style.opacity = "1"
   }
 
   const emptyCart = () => {
     document.querySelector(".list-group").innerHTML = ""
-    document.querySelectorAll(".card").forEach(card => card.style.border = "none")
+    document.querySelectorAll(".card").forEach(card => card.style.opacity = "1")
     const totale = document.querySelector("h1 span")
     totale.innerText = "0"
 }
 
-  const hideCard = (event) => document.querySelector(".col-12").remove();    
+  const hideCard = (event) => event.target.closest(".col-12").remove();    
